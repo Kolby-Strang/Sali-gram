2cb8268b9907ed51e9ad3ab728bf3d461c6ff34b
Cody <qtipcody@gmail.com>
2023-10-20 11:24:28 -0600

    can get posts

--- client/app/models/Post.js
+++ client/app/models/Post.js
@@ -5,7 +5,7 @@ export class Post {
         this.body = data.body
         this.image = data.image
         this.creatorId = data.creatorId
-        this.creator = data.create
+        this.creator = data.creator
     }
 
     get postCardTemplate() {
@@ -13,15 +13,15 @@ export class Post {
     <div class="col-4 mt-2">
         <div class="lizard-card">
         <img class="img-fluid"
-        src="${this.image}"
+        src="https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?auto=format&fit=crop&q=80&w=3024&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="${this.title}">
         <div class="d-flex justify-content-between align-items-center p-4">
         <h3>${this.title}</h3>
         <div class="d-flex justify-content-end align-items-center">
             <img class="rounded-circle user-image"
-            src="https://images.unsplash.com/photo-1528274725619-40604dc7dad7?auto=format&fit=crop&q=80&w=3570&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="${this.creator.picture}"
             alt="user">
-            <p class="mx-2">username</p>
+            <p class="mx-2">${this.creator.name}</p>
         </div>
         </div>
         <div class="pb-3 ps-4">
